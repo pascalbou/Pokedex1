01968ea539b605d654990356f2ec8febe8d8df7e
krikaz <pascal.bou@protonmail.com>
2020-08-15 09:49:12 +0200

    first lesson

---
+++ Pokedex1/Coordinator.swift
@@ -0,0 +1,38 @@
+//
+//  Coordinator.swift
+//  Pokedex1
+//
+//  Created by krikaz on 8/14/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import UIKit
+
+final class AppCoordinator {
+    // MARK: - Properties
+
+    private let window: UIWindow
+    private var navigationController: UINavigationController!
+
+    // MARK: - Initializers
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    // MARK: - CoordinatorType
+
+    func start() {
+        let viewController = ViewController(displayDetail: displayDetail)
+
+        navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+    func displayDetail(name: String) {
+        let viewController = DetailViewController(name: name)
+        
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
